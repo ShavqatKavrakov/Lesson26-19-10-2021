917559ff30ea660327cc65bc5a320c7f1eaecaf4
ShavqatKavrakov <kavrakovshavqatinfo@gmail.com>
2021-10-23 13:47:20 +0300

    change first commit

--- Lesson26-19-10-2021/Program.cs
+++ Lesson26-19-10-2021/Program.cs
@@ -11,36 +11,37 @@ namespace Lesson26_19_10_2021
             Random random = new Random(10);
             int n = random.Next();
             Console.WriteLine($"Number: {n}");
-            var reversmas = n.ToString().ToArray().Reverse();
+            var reversmas = n.ToString().ToArray().Reverse().ToArray();
             Console.Write("Reverse number: ");
-            foreach (var i in reversmas)
-                Console.Write($" {i} ");
+            WriteArray(reversmas);
             Console.Write($"\n\nArray: ");
             int[] mas = new[] { 1, 2, 3, 4, 5, 1, 2, -2, -3, -23, 4 };
             if (mas.Length == 0)
                 Console.WriteLine(mas);
-            foreach (var i in mas)
-                Console.Write($" {i} ");
+            WriteArray(mas);
             var count = mas.Where(p => p > 0).Count();
             var sum = mas.Where(mas => mas < 0).Sum();
             double[] result = new double[] { count, sum };
             Console.Write($"\nCount positive and Sum nagative : ");
-            foreach (var i in result)
-                Console.Write($" {i} ");
+            WriteArray(result);
             string[] array1 = new string[] { "Glasses", "Eyes", "Monocles", "Ere", "Rf" };
             Console.Write($"\n\nArray string: ");
-            foreach (var i in array1)
-                Console.Write($" {i} ");
+            WriteArray(array1);
             Console.Write($"\nSort for length of Array string: ");
-            var resultArray1 = array1.OrderBy(x => x.Length);
+            var resultArray1 = array1.OrderBy(x => x.Length).ToArray();
+            WriteArray(resultArray1);
             double[] array2 = new[] { -2, -2, 4.23, -2, -2, -2 };
             double a = array2[0];
             Console.Write($"\n\nArray double: ");
-            foreach (var i in array2)
-                Console.Write($" {i} ");
+            WriteArray(array2);
             var resultArray2 = array2.SkipWhile(w => w == a).Take(1);
             foreach (var i in resultArray2)
                 Console.Write($"\nResult: {i}");
+        }
+         static void WriteArray<T>(T[] array )
+        {
+            foreach (var item in array)
+                Console.Write($" {item} ");
         }
     }
 }
